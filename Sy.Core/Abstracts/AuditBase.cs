6301ce7e4d43dd6e9255e1e15f76124f3f3e2b07
senaOzturk <cakmakcisena@gmail.com>
2019-09-03 10:51:16 +0300

    .

---
+++ Sy.Core/Abstracts/AuditBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sy.Core.Abstracts
+{
+    // degişiklik tarihi , degişikligi yapan kullanıcı vs. gibi işlemleri takip etmek için açtık.
+    public abstract class AuditBase
+
+    {
+      
+        [StringLength(50)]
+        public string CreatedUser { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;       //oluşturuldugu tarihi otomatik olarak kaydetsin diye eşitledik
+        [StringLength(50)]
+        public string UpdateUser { get; set; }
+        public DateTime? UpdatedTime { get; set; }          //bos olabilir alan olsun diye soru işareti koyduk
+
+    }
+}
---
+++ Sy.Core/Abstracts/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sy.Core.Abstracts
+{
+    public interface  IEntity
+    {
+    }
+
+    public interface IEntity<TKey>      //diamond expression genericliği ifade ediyor. Tkey nesnesinin Id nesnesinin tipine gore tipi degişecek
+    {
+        TKey Id { get; set; }
+
+    }
+}
